599453cbe8820af5026f6204104a50c8aad01890
Edo Shor <edo.shor@pipl.com>
2016-11-11 04:16:41 +0200

    Migrations - seed db with supported content types

---
+++ migrations/data/seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO content_types (name) VALUES
+  -- Collection Types
+  ('DAILY_LESSON'),
+  ('SHABBAT_LESSON'),
+  ('WEEKLY_YH'),
+  ('CONGRESS'),
+  ('VIDEO_PROGRAM'),
+  ('LECTURE_SERIES'),
+  ('MEALS'),
+  ('HOLIDAY'),
+  ('PIKNIK'),
+  ('UNITY_DAY'),
+
+  -- Content Unit Types
+  ('LESSON_PART'),
+  ('LECTURE'),
+  ('CHILDREN_LESSON_PART'),
+  ('WOMEN_LESSON_PART'),
+  ('CAMPUS_LESSON'),
+  ('LC_LESSON'),
+  ('VIRTUAL_LESSON'),
+  ('YESHIVAT_HAVERIM'),
+  ('MEAL'),
+  ('VIDEO_PROGRAM_CHAPTER'),
+  ('FULL_LESSON'),
+  ('TEXT');
