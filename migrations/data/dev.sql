cc77debf47504d14e0d0bf809a3a7270f9ecadb9
Kolya Kavod <creature@bk.ru>
2016-11-26 10:35:15 +0000

    Add dev.sql for seeding minimal dev data such as user. Remove bad code.

---
+++ migrations/data/dev.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email) VALUES
+  -- Operator for operations.
+  ('operator@dev.com')
--- rest/rest.go
+++ rest/rest.go
@@ -18,40 +18,5 @@ func CaptureStartHandler(c *gin.Context) {
 	if c.BindJSON(&cs) == nil {
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	}
-
-	//var cs CaptureStart
-	//example := &CaptureStart{
-	//	Type:       "some_type",
-	//	Station:    "1.2.3.4",
-	//	User:       "username",
-	//	FileName:   "this/is/file.name",
-	//	CaptureID:  "13eA3b1341ff",
-	//}
-	//log.Infoln("Content Length:", c.Request.ContentLength);
-	//buf := new(bytes.Buffer)
-	//buf.ReadFrom(c.Request.Body)
-	//body := buf.String()
-	//log.Infoln("Body:", body);
-	//if err := json.Unmarshal([]byte(body), &cs); err == nil {
-	//	if cs.Type == "" || cs.Station == "" || cs.User == "" || cs.FileName == "" || cs.CaptureID == "" {
-	//		c.JSON(http.StatusBadRequest, gin.H{
-	//			"Error": "One or more required fields are empty.",
-	//			"Input": body,
-	//			"Example": example,
-	//		})
-	//		c.AbortWithStatus(http.StatusBadRequest)
-	//		return
-	//	}
-	//	c.JSON(http.StatusOK, gin.H{"status": "ok"})
-	//	return
-	//} else {
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"Error": "Could not parse JSON from input paylod.",
-	//		"Details": err.Error(),
-	//		"Example": example,
-	//	})
-	//	c.AbortWithStatus(http.StatusBadRequest)
-	//	return
-	//}
 }
 
