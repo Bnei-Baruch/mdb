0476b5121bec453005a93afb72b836104fa7589e
bbfsdev <bbfsdev@gmail.com>
2017-02-10 07:22:30 +0000

    Replace bad input request \"\" with \"

--- utils/middleware.go
+++ utils/middleware.go
@@ -7,7 +7,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+    "io/ioutil"
 	"net/http"
+    "regexp"
+    "strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -119,3 +122,26 @@ func GinBodyLogMiddleware(c *gin.Context) {
 		fmt.Println("Response body: " + blw.body.String())
 	}
 }
+
+func FixDoubleQuotesInInput(c *gin.Context) {
+    // TODO: Remove when input request fixed.
+    b, _ := ioutil.ReadAll(c.Request.Body)
+    body := string(b)
+    fmt.Printf("body:%s\n", body)
+    re := regexp.MustCompile("\"(\"[[:alnum:]]*\")\"")
+    if matches := re.FindStringSubmatch(body); len(matches) > 1 {
+        fmt.Printf("matches:%v\n", matches)
+        for _, m := range matches[1:] {
+            body = strings.Replace(
+                body,
+                fmt.Sprintf("\"%s\"", m),
+                fmt.Sprintf("%s", m),
+                1)
+        }
+    }
+    fmt.Printf("fixed:%s\n", body)
+    c.Request.Body = ioutil.NopCloser(bytes.NewReader([]byte(body)))
+
+    c.Next()
+}
+
