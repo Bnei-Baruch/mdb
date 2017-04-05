755a0aef811538492d02a71bd88c9eb8099b3eee
bbfsdev <bbfsdev@gmail.com>
2017-04-05 02:01:09 +0000

    Remove colon from migrations (for git in windows)

--- migrations/2017-01-02-04:50:34_tide_up.sql
+++
@@ -1,7 +0,0 @@
--- rambler up
-
-ALTER TABLE strings RENAME TO string_translations;
-
--- rambler down
-
-ALTER TABLE string_translations RENAME TO strings;
--- migrations/2017-03-15_04:29:09_files_content_unit_index.sql
+++
@@ -1,8 +0,0 @@
--- MDB generated migration file
-
--- rambler up
-CREATE INDEX IF NOT EXISTS files_content_unit_id_idx
-  ON files USING BTREE (content_unit_id);
-
--- rambler down
-DROP INDEX IF EXISTS files_content_unit_id_idx;
