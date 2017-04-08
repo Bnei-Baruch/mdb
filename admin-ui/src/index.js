ae25e0c21eb3d73721e5827f73fe6bb91f6a6c00
yanivel <yaniv.elimor@gmail.com>
2017-04-08 22:08:56 +0300

    add redux

--- admin-ui/src/index.js
+++ admin-ui/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import createStore from './redux/createStore';
 import App from './components/App/App';
 import './index.css';
 
+const store = createStore();
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
