eee859a97db9232d66ddccdd0dbd9f050a2c758b
Edo Shor <edoshor@gmail.com>
2017-05-03 13:13:45 +0300

    Deploy - send daily log warnings email

---
+++ misc/email_warnings.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set +e
+set -x
+
+DATE="$(date -d "yesterday 13:00" '+%Y-%m-%d')"
+#DATE="2017-04-30"
+
+LINES="$(egrep "${DATE}(.*) level=warning" /sites/mdb/logs/mdb.log)"
+
+if [ "$LINES" = "" ];then
+	echo "No warnings"
+	exit 0
+fi
+
+LC=$(echo "$LINES" | grep -c "^")
+
+echo "$LINES" | mail -s "MDB warnings $DATE [$LC]" -r "mdb@bbdomain.org" edoshor@gmail.com
+
