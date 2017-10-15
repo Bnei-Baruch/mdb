8ca1883aac2cac89b47fdb07f47ff0806c176acc
Edo Shor <edoshor@gmail.com>
2017-10-15 15:46:59 +0300

    bump version

--- version/version.go
+++ version/version.go
@@ -5,8 +5,8 @@ import "fmt"
 // See http://semver.org/ for more information on Semantic Versioning
 var (
 	Major      = 0
-	Minor      = 6
-	Patch      = 9
+	Minor      = 7
+	Patch      = 0
 	PreRelease = "" // Set this via makefile. See docs for more info on release process
 )
 
