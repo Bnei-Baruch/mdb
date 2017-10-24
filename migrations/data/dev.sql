57634d5612b3d39e1f4173c4aa3fe6a7486d0539
Edo Shor <edoshor@gmail.com>
2017-10-24 12:18:25 +0300

    Batch Convert - add db queue DDL for tests

--- migrations/data/dev.sql
+++ migrations/data/dev.sql
@@ -5,3 +5,12 @@ INSERT INTO users (email) VALUES
   ('operator2@dev.com'),
   ('operator3@dev.com'),
   ('operator@dev.com');
+
+
+DROP TABLE IF EXISTS batch_convert;
+CREATE TABLE batch_convert (
+  file_id       BIGINT REFERENCES files                          NOT NULL,
+  operation_id  BIGINT REFERENCES operations                     NULL,
+  request_at    TIMESTAMP WITH TIME ZONE                         NULL,
+  request_error TEXT                                             NULL
+);
