52352576f701a0c3cd6cf46fe00f6357705b8edd
davgur <davgur.ru@gmail.com>
2021-05-26 13:19:00 +0300

    code review

--- importer/cusource/remove_files_by_sha1.go
+++ importer/cusource/remove_files_by_sha1.go
@@ -15,7 +15,6 @@ import (
 	"github.com/volatiletech/sqlboiler/queries/qm"
 	"io"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -44,79 +43,73 @@ func run(mdb *sql.DB) {
 	log.Infof("Found %d files for remove", len(files))
 	removeCount := len(files)
 	for _, f := range files {
-		oIds := make([]string, 0)
+		oIds := make([]int64, 0)
 		for _, o := range f.R.Operations {
-			oIds = append(oIds, strconv.FormatInt(o.ID, 10))
+			oIds = append(oIds, o.ID)
 		}
 		err = deleteFileOnTransaction(mdb, f.ID, oIds)
 		if err != nil {
-			log.Infof("File uid %s was not removed", f.UID)
+			log.Debugf("File uid %s was not removed", f.UID)
 			removeCount--
 			continue
 		}
 	}
 	log.Infof("All procceses ended. Removed %d files", removeCount)
 }
-func deleteFileOnTransaction(mdb *sql.DB, fId int64, oIds []string) error {
+func deleteFileOnTransaction(mdb *sql.DB, fId int64, oIds []int64) error {
 	log.Info("Open transaction")
 	tx, err := mdb.Begin()
+	if err != nil {
+		log.Errorf("Problem on create transaction: %s ", err)
+		return err
+	}
+
 	defer func() {
 		if err == nil {
 			log.Info("Send commit of transaction ")
 			err = tx.Commit()
 			if err != nil {
-				log.Infof("Commit error %s", err)
+				log.Errorf("Commit error %s", err)
 			}
 		} else {
 			log.Info("Send Rollback of transaction ")
 			err = tx.Rollback()
 			if err != nil {
-				log.Infof("Commit error %s", err)
+				log.Errorf("Commit error %s", err)
 			}
 		}
 	}()
 
-	if err != nil {
-		log.Infof("Problem on create transaction: %s ", err)
-		return err
-	}
-
 	log.Infof("Start remove files_operations with files ids:%v ", fId)
 	qfo := fmt.Sprintf("DELETE FROM files_operations fo where  fo.file_id = %d", fId)
-	rfo, err := queries.Raw(mdb, qfo).Query()
+	_, err = queries.Raw(mdb, qfo).Exec()
 	if err != nil {
-		log.Infof("Problem on delete files_operations: %s ", err)
+		log.Errorf("Problem on delete files_operations: %s ", err)
 		return err
 	}
-	defer rfo.Close()
 
 	log.Infof("Start remove operations with ids: %v ", oIds)
 
 	if len(oIds) > 0 {
-		qo := fmt.Sprintf("DELETE FROM operations o where  o.id IN (%s)", strings.Join(oIds, ","))
-		ro, err := queries.Raw(mdb, qo).Query()
+		err = models.Operations(mdb, qm.WhereIn("id IN ?", utils.ConvertArgsInt64(oIds)...)).DeleteAll()
 		if err != nil {
-			log.Infof("Problem on delete operations: %s ", err)
+			log.Errorf("Problem on delete operations: %s ", err)
 			return err
 		}
-		defer ro.Close()
 	}
 
 	qfs := fmt.Sprintf("DELETE FROM files_storages fs where  fs.file_id = %d", fId)
-	rfs, err := queries.Raw(mdb, qfs).Query()
+	_, err = queries.Raw(mdb, qfs).Exec()
 	if err != nil {
-		log.Infof("Problem on delete files_storages: %s ", err)
+		log.Errorf("Problem on delete files_storages: %s ", err)
 		return err
 	}
-	defer rfs.Close()
 
-	qf := fmt.Sprintf("DELETE FROM files f where  f.id = %d", fId)
-	rf, err := queries.Raw(mdb, qf).Query()
+	err = models.Files(mdb, qm.WhereIn("id = ?", fId)).DeleteAll()
 	if err != nil {
-		log.Infof("Problem on delete files: %s ", err)
+		log.Errorf("Problem on delete files: %s ", err)
 		return err
 	}
-	defer rf.Close()
 
 	log.Info("Delete successful and committed transaction ")
 	return nil
